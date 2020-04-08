2c7b2a5f35be7781f2718cd29e86cc240c478b75
Al-Shubei Mokhtar (uid21527) <Mokhtar.Al-Shubei-EXT@continental-corporation.com>
2020-04-09 01:54:52 +0200

    portsWhitelist

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,39 +15,99 @@ class AppComponent extends React.Component {
     }
 
     componentDidMount() {
-		fetch("../dataSmall.json")
+		//fetch("../dataSmall.json")
+		fetch("../dataLarge.json")
 		.then((response) => response.json())
-		.then((data) => this.setState({ "partitions": data.partitions }))
-		
+		.then((data) => this.setState({ "partitions": data.partitions , "portsWhitelist": data.portsWhitelist}))
     }
 
 	render() {		
-		const clustersDiagram = this.state.partitions.map((partition, dx)=>{
-			return <div className="item" key={dx}>
-					{partition.name}
-					<div className="container">						
-						{(partition.clusters || []).map((cluster, dx)=>{return <div className="item" key={dx}>						
-							{cluster.name}
-							<div className="container">
-								{(cluster.components || []).map((component, dx)=>{return <div className="item" key={dx}>
-								{component.name}
-									<div className="container">
-										{(component.ports || []).map((port, dx)=>{
-											return <div key={dx}>{port.name}</div>
-										})}
-									</div>			
-								</div>})}
-							</div>	
-						</div>})}
-					</div>				
-				</div>;
+		const state = this.state
+		const partitionDiagrams = this.state.partitions.filter(p=>isRtePartition(p, state)).map((partition, dx)=>{
+			return <PartitionDiagram partition={partition}  key={dx}  state={state}/>;
 		});
 
-		return (<div className = "index">			
-			<div className = "container">{clustersDiagram}</div></div>);
+		return (
+			<div className = "fbx partitions">{partitionDiagrams}</div>
+			);
 	}
 }
 
 AppComponent.defaultProps = {};
 
 export default AppComponent;
+
+
+
+
+const PartitionDiagram = (props, key) => {
+	const { partition, state } = props
+	return 	<div className="item partition" key={key}>
+				{partition.name}
+				<div className="fbx clusters">						
+					{(partition.clusters || []).filter(c=>isRteCluster(c, state)).map((cluster, dx)=>{
+						return <ClusterDiagram cluster={cluster} key={dx} state={state}  />})}
+				</div>				
+			</div>
+}
+
+const ClusterDiagram = (props, key) => {
+	const { cluster, state } = props
+	let Diagrams = (cluster.components || []).filter(c=>isRteComponent(c, state)).map((component, dx)=>{
+		return <ComponentDiagram component={component} key={dx}  state={state} />})
+	return <div className="item cluster" key={key}>						
+				{cluster.name}
+				<div className="fbx components">
+					{Diagrams}
+				</div>	
+			</div>
+}
+
+const ComponentDiagram = (props, key) => {
+	const { component, state } = props
+	return <div className="item component">					
+				{component.name}
+				<div className="fbx ports">
+					{(component.ports || []).filter(p=>isRtePort(p, state)).map((port, dx)=>{
+						return <PortDiagram port={port} key={dx} index={dx} state={state} />
+					})}
+				</div>			
+			</div>
+}
+
+const PortDiagram = (props, key) => {
+	const { port, state, index } = props	
+	if (state.portsWhitelist && state.portsWhitelist.length && !state.portsWhitelist.find(n=>n==port.name)) {
+		return null
+	}
+	const vdps = port.vdps.filter(v=>isRteVdp(v, state)).map((vdp, dx)=><Vdp vdp={vdp} key={dx} state={state} index={dx} />)
+	return <div className="item port">
+		<div className="tooltip" key={key} >{'p'+(index+1)}<span className="tooltiptext">{port.name}</span></div> 
+		{vdps}
+	</div>
+}
+
+const Vdp = (props, key) => {
+	const { vdp, state, index } = props	
+	return <div className="item vdp tooltip" key={key} >{'vdp'+(index+1)}<span className="tooltiptext">{vdp.name}</span></div>
+}
+
+const isRtePartition = (p, state) => {
+	return  p && p.clusters && p.clusters.length && p.clusters.find(c=>isRteCluster(c, state))
+} 
+
+const isRteCluster = (c, state) => {
+	return  c && c.components && c.components.length && c.components.find(c=>isRteComponent(c, state))
+} 
+
+const isRteComponent = (c, state) => {	
+	return  c && c.ports && c.ports.length && c.ports.find(p=>isRtePort(p, state))
+}
+
+const isRtePort = (p, state) => {
+	return p && ((!state.portsWhitelist || !state.portsWhitelist.length) || state.portsWhitelist.find(n=>n==p.name))
+}
+
+const isRteVdp = (vdp, state) => {
+	return vdp && ((!state.portsWhitelist || !state.portsWhitelist.length) || state.portsWhitelist.find(n=>n==vdp.name))
+}
